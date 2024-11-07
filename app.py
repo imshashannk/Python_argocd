ee2ba7da322bd9046dd44663556415da51f95552
Shashank Khankari <81563774+imshashannk@users.noreply.github.com>
2024-11-07 12:10:21 +0530

    Update app.py
    added random data

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! ndflkghkladmvkabeofjdl;"
+    return "Hello, World! whysvl;alkvdpoalwekbfaodcpiolnejbfhodipckl;mnvbrhoiefpkdls;am,n evuoipwqflk;dcnrhuiofkjlndsacbkseifojkdsn"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
