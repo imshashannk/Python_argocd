da1f78d1500a29593e78e4cc3cae4f39e8639863
Shashank Khankari <81563774+imshashannk@users.noreply.github.com>
2024-11-15 19:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "D"
+    return "Home path"
 
 @app.route("/ingress")  # Add a new route to call ingress function
 def ingress():
